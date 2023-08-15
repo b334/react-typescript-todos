66976ce4addfec36be0b56f6783d9fca9dcaa611
bishal <bishals10@gmail.com>
2023-08-16 01:12:29 +0300

    add functionality to remove todo-item when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
+import { prettyDOM } from "@testing-library/react";
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -9,10 +10,15 @@ function App() {
     const newTodo = new Todo(todoText);
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((prevTodo) => prevTodo.id !== todoId));
+  };
+
   return (
     <>
       <NewTodo onAddTodo={onAddTodoHandler} />
-      <Todos items={todos} />;
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />;
     </>
   );
 }
